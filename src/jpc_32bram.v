47a9a453176d1c03fe33e1dde566fa00a1099d05
Jean Mousinho <jean.mousinho@gmail.com>
2025-03-09 20:30:48 +0000

    Added support to constant value in BRAM.

--- src/jpc_32bram.v
+++ src/jpc_32bram.v
@@ -14,9 +14,13 @@ module jpc_32bram #(
     reg [`JPC_ADDRESS_WIDTH-1:0] mem [0:DEPTH-1];
 
     // Read from memory file (if defined)
+    integer i;
     initial begin
+        for (i = 0; i < DEPTH; i = i + 1) begin
+            mem[i] = `JPC_MEM_DEFAULT_VALUE;
+        end
         `ifdef JPC_IMEM_FILE
-            $readmemh(`JPC_IMEM_FILE, mem); // Read .mem file in hexadecimal format
+            $readmemh(`JPC_IMEM_FILE, mem);
         `else
             $display("No memory file provided. Using default values.");
         `endif
